1dc6cf2b8f65b1961ba594bd72c9bb1fdb8dd754
JPark110 <jeremy.park@csuglobal.edu>
2024-09-22 20:24:37 -0500

    Added Main.java with Buffalo William's info

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare variables for personal information
+        String firstName = "Buffalo";
+        String lastName = "William";
+        String streetAddress = "112233 56th St";
+        String city = "Nashville";
+        String zipCode = "37208";
+        
+        // Print each item on a new line
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
